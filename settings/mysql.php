3d0ecc658fd8e773da989582e9dd36cbac074a7b
Simon Engelbracht <dermetelgameryt@gmail.com>
2023-05-21 01:10:15 +0200

    MySQL Connection

--- settings/mysql.php
+++ settings/mysql.php
@@ -1,3 +1,8 @@
 <?php 
+    $dsn = 'mysql:dbname=metelplus;host=localhost';
+    $username = 'root';
+    $password = '';
     
+    $con = new PDO($dsn, $username, $password);
+    $con->exec("CREATE TABLE IF NOT EXISTS users (id INT(8) NOT NULL AUTO_INCREMENT,firstname VARCHAR(255) NOT NULL,lastname VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL,PRIMARY KEY (id))");
 ?>
