c6b870e94d9c345e9e8f606a56a4ee7d4b38886b
Simon Engelbracht <dermetelgameryt@gmail.com>
2023-05-21 01:10:30 +0200

    Added Header & Footer template

---
+++ settings/templates/header.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MetelPlus</title>
+    <link rel="icon" type="image/x-icon" href="../settings/webmedia/metflix_logo.png">
+    <link rel="stylesheet" href="../settings/css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<style>
+body {
+    background-color: #141414;
+    color: white;
+}
+
+.custommetelbutton {
+    background: linear-gradient(45deg, #b1060f, #af1b24, #530307);
+    color: white;
+}
+</style>
+<body>
