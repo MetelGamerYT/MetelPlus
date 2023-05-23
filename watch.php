83b3d1c174cd77cf8a68971d75ee9b41e109ea45
Simon Engelbracht <dermetelgameryt@gmail.com>
2023-05-23 19:18:01 +0200

    Added Movie Video + Title

---
+++ watch.php
@@ -0,0 +1,44 @@
+<?php 
+    if(session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    if(!isset($_SESSION["ID"])) {
+        header("Location: core/login.php");
+    }
+    require "settings/templates/header.php";
+    require "settings/mysql.php";
+?>
+<script src="https://kit.fontawesome.com/b7c91fdb59.js" crossorigin="anonymous"></script>
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><img src="./settings/webmedia/metelplus.png" height="40"></a>
+    <button class="navbar-toggler custommetelbutton" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="index.php"><i class="fa-solid fa-house"></i> Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="search.php"><i class="fa-solid fa-magnifying-glass"></i> Search</a>
+            </li>
+        </ul>
+    </div>
+  </div>
+</nav>
+<br>
+<?php         
+    $stmt = $con->prepare("SELECT * FROM movies WHERE id = '".$_GET["look"]."'");
+    $stmt->execute();
+    while($row = $stmt->fetch()) { ?>  
+        <h1 class="text-center"><?php echo $row["movie_name"]; ?></h1>
+        <div class="container text-center">
+            <video class="w-100" controls>
+                <source src="media/<?php echo $row["movie_filename"] ?>" type="video/mp4">
+                Your browser does not support the video tag.
+            </video> 
+        </div>
+    <?php } 
+require "settings/templates/footer.php";
+?>
